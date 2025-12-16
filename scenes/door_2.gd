2faac2e1c9c67875fbf058da471767fe2b28a92a
Jono-Lav <yonattan2907@gmail.com>
2025-12-16 18:10:37 +0200

    hous

--- scenes/door_2.gd
+++ scenes/door_2.gd
@@ -4,18 +4,22 @@ extends Interactable
 @onready var anim = $AnimationPlayer2
 @onready var open : AudioStreamPlayer3D = $Open
 @onready var close : AudioStreamPlayer3D = $Close
+@onready var door = $"."
 
 var opened = false
 
 func interact(body):
+	if anim.is_playing():
+		return
 	if !opened:
 		open.play()
 		opened = true
 		anim.play("open")
-		collision.disabled = true
+		await anim.animation_finished
+		door.prompt = "Close"
 	else:
 		close.play()
 		opened = false
 		anim.play("close")
-		collision.disabled = false
-		
+		await anim.animation_finished
+		door.prompt = "Open"
