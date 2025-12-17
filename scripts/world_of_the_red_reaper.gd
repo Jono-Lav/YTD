3a58e66e2bf7631f12e8e24787fba7036418cd25
Jono-Lav <yonattan2907@gmail.com>
2025-12-18 01:29:57 +0200

    uncharted orb

--- scripts/world_of_the_red_reaper.gd
+++ scripts/world_of_the_red_reaper.gd
@@ -1,8 +1,5 @@
 extends Mission
 
-@onready var shaveranim : AnimationPlayer = $TheShaver/AnimationPlayer2
-@onready var rotem : AnimationPlayer = $RedReaper/AnimationPlayer2
-@onready var rotemInteractableCollision : CollisionShape3D = $RedReaper/StaticBody3D/CollisionShape3D
 @onready var TransitionAnim : AnimationPlayer = $Transition
 
 func _ready() -> void:
